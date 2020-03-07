b267638232c31b1d4f6307a23c1622aabf3dccf3
Daniel Brenner <dabrenne@ucsc.edu>
2020-03-06 17:25:51 -0800

    updated CozirTest

--- LibrariesAndTestHarnesses/Co2LibrarytTest/Co2LibrarytTest.ino
+++ LibrariesAndTestHarnesses/Co2LibrarytTest/Co2LibrarytTest.ino
@@ -1,4 +1,5 @@
 #include "CozirLib.h"
+#include "SPI.h"
 //#include "SdCard.h"
 
 void setup() {
@@ -8,33 +9,45 @@ void setup() {
    while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
-Serial.println("Initializing COzIR");
+  Serial.println("Initializing COzIR");
+  delay(10000);
   uint8_t errorCheck = Cozir_Init();
   if(errorCheck == 0){
-    Serial.println("Failure");
+    Serial.println("Failure on Serial1");
   }else if (errorCheck == 1){
-    Serial.println("Success");
+    Serial.println("Success on Serial1");
   }else {
-    Serial.println("Fuck");
+    Serial.println("Serious Failure");
+  }
+  errorCheck = Cozir_Calibrate();
+  if(errorCheck == 0){
+    Serial.println("Calibration Failure");
+  }else if (errorCheck == 1){
+    Serial.println("Calibration Success");
+  }else {
+    Serial.println("Serious Failure");
   }
 
-//  Serial1.setTX(1);
-//  Serial1.setRX(0);
-//  Serial1.begin(9600, SERIAL_8N1);
-//  delay(3000);
-//
-//  Serial1.print("K 2\r\n");
-//  delay(500);
-//  Serial1.print("z\r\n");
+//  errorCheck = Cozir2_Init();
+//  if(errorCheck == 0){
+//    Serial.println("Failure on Serial2");
+//  }else if (errorCheck == 1){
+//    Serial.println("Success on Serial2");
+//  }else {
+//    Serial.println("Serious Failure");
+//  }
 
 }
 
 elapsedMillis sinceLastReading;
 elapsedMillis curTime;
-Data cozirData;
+CozirData cozirData;
+CozirData cozirData2;
 char rxVal = 0;
 char myString[100];
 unsigned int holder;
+
+//comment out this loop if using two sensors
 void loop() {
   if (sinceLastReading > 550){
     sinceLastReading = 0;
@@ -53,3 +66,29 @@ void loop() {
   
 
 }
+
+//comment either this loop or the one above depending on if one or two sensors are being used
+//this loop will only print give co2 data and time in ms
+//void loop() {
+//  if (sinceLastReading > 550){
+//    sinceLastReading = 0;
+//    Cozir_Request_Data();
+//    Cozir2_Request_Data();
+//  }
+//
+//  if(Cozir_NewDataAvailable() == 1 && Cozir2_NewDataAvailable() == 1){
+//    cozirData.co2 = Cozir_Get_Co2();
+//    cozirData.temp = Cozir_Get_Temp();
+//    cozirData.rh = Cozir_Get_Rh();
+//
+//    cozirData2.co2 = Cozir2_Get_Co2();
+//    cozirData2.temp = Cozir2_Get_Temp();
+//    cozirData2.rh = Cozir2_Get_Rh();
+//    
+//    holder = curTime;
+//    sprintf(myString, "%d\t%d\t%d", holder, cozirData.co2, cozirData2.co2);
+//    Serial.println(myString);
+//  }
+//  
+//
+//}
