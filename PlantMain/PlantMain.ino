eaf953b994468cc987be5440c8a805f5cbb01bcd
Daniel Brenner <dabrenne@ucsc.edu>
2020-03-06 19:02:34 -0800

    added main

---
+++ PlantMain/PlantMain.ino
@@ -0,0 +1,15 @@
+#include "PlantMeterHSM.h"
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  InitHSM();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  while(1);
+}
