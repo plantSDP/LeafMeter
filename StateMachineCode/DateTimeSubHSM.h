19e4f68826c1969cbbbb9e2ce8239f3f04f628b9
plantSDP <60907971+plantSDP@users.noreply.github.com>
2020-03-13 06:20:36 -0700

    create sub-state header file for DateTime

---
+++ StateMachineCode/DateTimeSubHSM.h
@@ -0,0 +1,20 @@
+#ifndef SUB_HSM_DATETIME_H
+#define SUB_HSM_DATETIME_H  
+
+/*******************************************************************************
+ * PUBLIC #INCLUDES                                                            *
+ ******************************************************************************/
+
+#include "Configure.h"   // defines events
+#include "Arduino.h"
+
+
+/*******************************************************************************
+ * PUBLIC FUNCTION PROTOTYPES                                                  *
+ ******************************************************************************/
+
+uint8_t Init_SubHSM_DateTime(void);
+
+Event Run_SubHSM_DateTime(Event thisEvent);
+
+#endif /* SUB_HSM_DATETIME_H */
