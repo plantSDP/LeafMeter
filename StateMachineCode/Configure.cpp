9509a58b417629a122118088dd3dd1806d74376f
Jonathan Nguyen <plantseniordesignteam2019@gmail.com>
2020-05-29 13:55:35 -0700

    working with remote

--- StateMachineCode/Configure.cpp
+++ StateMachineCode/Configure.cpp
@@ -1,45 +1,5 @@
 #include "Configure.h"
 
-// Global Variable/Array/Struct **definitions**. Declarations are in the .h file.
-char myString[50];
-
-char fileName[33];                  // global strings for filenames
-char metaFileName[38];
-
-uint8_t rfOption = RF_NO;       // sets rfOption, default is NO (0)
-
-uint8_t period = 60;            // holds value for period in between measurements in [min], default is 60[min] but can be user-defined
-uint8_t numCycles = 1;          // holds value for number of measurement cycles, default is 1
-uint8_t numSamples = 0;         // holds value of the number of samples taken in a single active meas cycle, default is 0
-
-uint16_t co2 = 0;		// holds CO2 measurement in ppm from CozIR, default is 0
-uint8_t hum = 0;		// holds humidity measurement in RH from CozIR, default is 0
-int8_t temp = 0;	    // holds temperature mesurement in C from CozIR, default is 0
-int pres = 0;			// holds pressure measurement in Pa from BME280, default is 0
-uint16_t lux = 0;		// holds light measurement in lux from TSL2591, default is 0
-
-uint16_t co2Data[MAX_SAMPLES_PER_CYCLE/5];			// holds co2 data for one meas cycle
-
-uint8_t dataArrayAddress;
-
-
-uint8_t month1 = 0;	// MM/DD/YY, default 01/01/20
-uint8_t month2 = 1;
-
-uint8_t day1 = 0;
-uint8_t day2 = 1;
-
-uint8_t year1 = 2;
-uint8_t year2 = 0;
-
-uint8_t hour1 = 0;	// HH:MM millitary time, default 00:00
-uint8_t hour2 = 0;
-
-uint8_t min1 = 0;
-uint8_t min2 = 0;
-
-ts rtcDateTimeStruct; // RTC struct, holds date and time values and is used for setting/retrieving this data from the RTC
-
 //==============================
 // Public Function definitions
 //==============================
